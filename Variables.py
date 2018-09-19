b1a7ffab176aabc9c881821383d555de53b6fb2d
Hadj Koiuder Mohamed <43360517+akoka47@users.noreply.github.com>
2018-09-19 20:39:54 +0100

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# programming by hk.mohamed
+# lesson _ variables 
+
+var_num = 100
+
+print(var_num)
+
+var_oth= var_num / 5.3
+
+print(var_oth)
+
+_100ma = 5
+
+print(_100ma)
---
+++ strings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# programming by hk.mohamed
+# lesson _ string 
+
+print('Hello World')
+
+print("Double quotes")
+
+print('concatena'+'tion')
+
+print('hello','there')
+
+print('I\'m 5')
+
+print("I'm 5")
+
+print('He said "hi"')
+
+
+
+
+
