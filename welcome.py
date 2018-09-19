3e0c61b426eafcccc4abbbb67a05ab01c0fbd67f
Hadj Koiuder Mohamed <43360517+akoka47@users.noreply.github.com>
2018-09-19 21:54:33 +0100

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
-# programming by hk.mohamed
+# programming by: hk.mohamed
+#lesson _ output
     
 print('welcome to python') # this code for print 'welcome to python' output in console
