e038a2f3ca5a7eea72982d0e7fda429800e84e75
Hadj Koiuder Mohamed <43360517+akoka47@users.noreply.github.com>
2018-09-19 20:47:55 +0100

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# programming by hk.mohamed
+# lesson_ while loop
+
+
+condition = 1
+
+while condition < 10:
+    print(condition)
+    
+    '''
+    multiple
+    lines
+    can
+    go
+    here
+    '''
+    
+    #condition = condition + 1
+    condition += 1
+
+condition = 5
+
+#while condition < 15:
+#    print('True')
+
+while True:
+    print('infinite')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
