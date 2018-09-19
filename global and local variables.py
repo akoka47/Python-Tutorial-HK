5605cc39a030749a53c7cc15494303efd9c62a1e
Hadj Koiuder Mohamed <43360517+akoka47@users.noreply.github.com>
2018-09-19 20:52:34 +0100

    Add files via upload

---
+++ global and local variables.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# programming by hk.mohamed
+# lesson _ global and local variables
+x = 6
+
+def example3():
+    global x
+    x += 1
+    print(x)
+
+def example():
+    z = 5
+    print(z)
+
+# cannot do this:
+#print(z)
+
+def example2():
+    z = 7
+    print(z)
+    y = x + 1
+    print(y)
+    return y
+
+x = example2()
+print(x)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
